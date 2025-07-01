92d2ff1eeb32594cb22fc6b9c3f5e77a48b26b2f
Guilherme Sartori <guilherme.2023014204@alunocesul.com>
2025-07-01 20:51:22 -0300

    feat: implementado metodos no bookRepository e modificado nome do Docker-compose

--- src/books/books.repository.ts
+++ src/books/books.repository.ts
@@ -1,6 +1,55 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "prisma.service";
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'prisma.service';
 
 @Injectable()
-export class ProfileRepository {
+export class BookRepository {
   constructor(private prisma: PrismaService) {}
+
+  async create(
+    book: Prisma.BookUncheckedCreateInput,
+  ): Promise<Prisma.BookUncheckedCreateInput> {
+    return await this.prisma.book.create({
+      data: book,
+    });
+  }
+
+  async findAll(): Promise<Prisma.BookUncheckedCreateInput[] | null> {
+    return await this.prisma.book.findMany();
+  }
+
+  async findById(id: string): Promise<Prisma.BookUncheckedCreateInput | null> {
+    return await this.prisma.book.findUnique({
+      where: { id },
+    });
+  }
+
+  async findByTitle(
+    title: string,
+  ): Promise<Prisma.BookUncheckedCreateInput | null> {
+    return await this.prisma.book.findUnique({
+      where: { title },
+    });
+  }
+
+  async findByISBN(
+    isbn: string,
+  ): Promise<Prisma.BookUncheckedCreateInput | null> {
+    return await this.prisma.book.findUnique({
+      where: { isbn },
+    });
+  }
+
+  async save(data: Prisma.BookUncheckedUpdateInput): Promise<void> {
+    await this.prisma.book.update({
+      where: { id: data!.toString() },
+      data: data,
+    });
+  }
+
+  async delete(book: Prisma.BookUncheckedCreateInput): Promise<void> {
+    await this.prisma.book.delete({
+      where: { id: book.id?.toString() },
+    });
+  }
+}
