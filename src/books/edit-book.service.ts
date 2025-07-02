7b03d488ddf5f9fbcbd0fecfbf396df8f116876e
Guilherme Sartori <guilherme.2023014204@alunocesul.com>
2025-07-01 21:21:09 -0300

    criado edit-books.service e find-by-id.service

---
+++ src/books/edit-book.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BookRepository } from './books.repository';
+
+interface EditBookServiceRequest {
+  id: string;
+  title: string;
+  author: string;
+  publicationYear: number;
+  isbn: string;
+}
+
+@Injectable()
+export class EditProductService {
+  constructor(
+    private bookRepository: BookRepository,
+  ) {}
+
+  async execute({
+    id,
+    title,
+    author,
+    publicationYear,
+    isbn,
+  }: EditBookServiceRequest): Promise<void> {
+    const book = await this.bookRepository.findById(id);
+
+    if (!book) {
+      throw new NotFoundException("Book not found.");
+    }
+
+    await this.bookRepository.save({
+      id,
+      title,
+      author,
+      publicationYear,
+      isbn,
+    });
+  }
+}
